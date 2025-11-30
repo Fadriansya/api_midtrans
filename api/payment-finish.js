6b220106270916b70a0362430abdfe98fd8e0be9
Fadriasya <nasrullahakbar18@gmail.com>
2025-11-30 10:19:22 +0700

    coba tes

--- api/payment-finish.js
+++ api/payment-finish.js
@@ -1,7 +1,7 @@
 // api/payment-finish.js
 module.exports = (req, res) => {
   const orderId = req.query.order_id || "";
-  const scheme = process.env.CUSTOM_FINISH_SCHEME || "myapp://payment";
+  const scheme = "http://api-midtrans-teal.vercel.app/api/payment-finish";
   const redirect = `${scheme}/success?order_id=${encodeURIComponent(orderId)}`;
   res.writeHead(302, { Location: redirect });
   res.end();
